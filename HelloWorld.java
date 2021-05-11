400bac210e3d065dc76fb0dd4b39fe6b4ac6ad16
azeem pasha <82228574+tech-engg@users.noreply.github.com>
2021-05-12 01:56:16 +0530

    again modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,8 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello the World");
 //<//<<<<<< HEAD
-//		System.out.println("Hello the java");
+		//System.out.println("Hello the java");
 //=======
 		//System.out.println("Hello java");
 //>>>>>>> 2e8089714bff2eb18699f92c3719f44eae9de2d5
