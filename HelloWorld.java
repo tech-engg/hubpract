80d2959b5f165f5ff96dea1d39973a1a3cb56c59
azeem pasha <82228574+tech-engg@users.noreply.github.com>
2021-05-12 02:12:18 +0530

    added how r u

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello  the World");
+		System.out.println("Hello  how are u");
 //<//<<<<<< HEAD
 		//System.out.println("Hello the java");
 //=======
