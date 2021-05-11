2e8089714bff2eb18699f92c3719f44eae9de2d5
azeem pasha <82228574+tech-engg@users.noreply.github.com>
2021-05-12 01:32:22 +0530

    made change

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello");
+		System.out.println("Hello java");
 		// TODO Auto-generated method stub
 
 	}
