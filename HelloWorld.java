79ce5a6914bceedb6a9e30b6b83da2900644600d
tech-engg <techsolution.enggn@gmail.com>
2021-05-12 01:24:24 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.javahubpract;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
